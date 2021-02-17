d01496ff06edc144ae25e712c51bea6af8b2d891
Konstantin <konstkam@mail.ru>
2021-02-17 21:05:44 +0300

    Working with Babel, core-js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var Promise = require('es6-promise-polyfill').Promise;
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
